ac214d731ffe464f6af0c356102bb835b07f0fd2
t <t.com>
2019-07-11 17:13:22 +0800

    update for androidx

--- android/src/main/java/com/rnbiometrics/ReactNativeBiometricsDialog.java
+++ android/src/main/java/com/rnbiometrics/ReactNativeBiometricsDialog.java
@@ -8,7 +8,7 @@ import android.content.DialogInterface;
 import android.hardware.fingerprint.FingerprintManager;
 import android.os.Build;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
+import androidx.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
